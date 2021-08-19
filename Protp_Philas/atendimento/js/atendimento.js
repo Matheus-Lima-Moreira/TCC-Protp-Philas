70560ba3a388741e7cef95f211deb2469e1223bd
Luis Guerra <86413741+Luis-Guerra-Ocarinna@users.noreply.github.com>
2021-08-19 06:05:09 -0300

    Mudanças 2021-08-04: atualização do .htaccess e mudança de pasta e nome pros users e schedules e seus js

---
+++ Protp_Philas/atendimento/js/atendimento.js
@@ -0,0 +1,48 @@
+/// <reference path="../../../typings/globals/jquery/index.d.ts" />
+// TODO: Documentar tudo (variavel funcao...)
+
+var url = "http://localhost/TCC/Protp_Philas";
+
+var slcMotivo;
+
+var edtOutro;
+var edtData;
+var edtAtendido;
+var edtAtendente;
+
+var divParticipantes;
+
+var btnDef;
+var btnOk;
+
+$(function () {
+  slcMotivo = $("#slcMotivo");
+
+  edtOutro = $("#edtOutro");
+  edtData = $("#edtData");
+  edtAtendido = $("#edtAtendido");
+  edtAtendente = $("#edtAtendente");
+
+  divParticipantes = $("#Participantes");
+
+  btnDef = $("#btnDef");
+  btnOk = $("#btnOK");
+
+  // TODO: validar as datas e outros, tem que apagar os campos quando sumir, talvez na hora de submitar
+  // Motivo Outro
+  slcMotivo.on("change", function () {
+    if ($(this).find(":selected").val() === "") { // "" => outro
+      edtOutro.show(); // edtOutro.css("display", "block")
+      edtData.hide();
+    } else {
+      edtOutro.hide(); // edtOutro.css("display", "none")
+      edtData.show();
+      // alert($(this).find(":selected").data("tempoPrevisto")); pegar o tempo previsto lol
+    }
+  });
+
+  // Def Part
+  btnDef.click(function () {
+    divParticipantes.toggle();
+  });
+});
---
+++ Protp_Philas/usuario/usuarios.php
@@ -0,0 +1,83 @@
+<?php
+//TODO: responder com cabeçalho
+include_once "connection.php";
+
+switch ($_SERVER["REQUEST_METHOD"]) {
+    case "GET":
+        if (isset($_REQUEST["GET"])) {
+            // header("HTTP/1.1 200 OK");
+            http_response_code(200);
+
+            $get = $_REQUEST["GET"];
+
+            echo $get;
+        } else {
+            echo "GET";
+        }
+
+        break;
+    case "POST":
+        if ($input = file_get_contents("php://input")) {
+            header("HTTP/1.1 201 Created");
+            // http_response_code(201);
+            header("Content-Type: application/json", false);
+
+            $post = json_decode($input, true);
+
+            $sql ='INSERT INTO
+            `tb_usuario` (`nome`, `telefone`, `cpf`, `login`, `senha`, `email`, `tipo`)
+            VALUES
+            (
+                "_nome",
+                "_telefone",
+                "_cpf",
+                "_login",
+                "_senha",
+                "_email",
+                "_tipo"
+            )';
+
+            // $sql = mysqli_real_escape_string($conn, strtr($sql, $post));
+
+            echo mysqli_query($conn, strtr($sql, $post));
+
+            // echo mysqli_error($conn); || mysqli_errono($conn);
+
+            mysqli_close($conn);
+
+            echo json_encode($post);
+            // echo $input;
+        } else {
+            echo "POST";
+        }
+
+        break;
+    case "PUT":
+        if ($input = file_get_contents("php://input")) {
+            // header("HTTP/1.1 202 Accepted");
+            http_response_code(202);
+
+            $put = json_decode($input);
+            echo $put->PUT;
+        } else {
+            echo "PUT";
+        }
+
+        break;
+    case "DELETE":
+        if (isset($_REQUEST["DELETE"])) {
+            // header("HTTP/1.1 202 Accepted");
+            http_response_code(202);
+
+            $del = $_REQUEST["DELETE"];
+
+            echo $del;
+        } else {
+            echo "DELETE";
+        }
+
+        break;
+    default:
+        # code...
+        break;
+}
